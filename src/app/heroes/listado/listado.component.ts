4cc26c05cdae27e4c050b1f1a7b2e5c561d5bd68
Gisel Cupul <gisel@gdalab.com>
2022-07-12 18:02:50 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent{
+  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor'];
+  heroeBorrado: string = '';
+
+  borrar() {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
